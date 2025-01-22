838dda38bcb7e9559427f71ef844466d489fa9da
oltha <olivier.thabet@polymtl.ca>
2025-01-22 15:17:13 -0500

    dsdss

--- INF2610_TP1/Section2_Processes/processes_part1.c
+++ INF2610_TP1/Section2_Processes/processes_part1.c
@@ -23,11 +23,11 @@ void question1() {
 
     pid1 = fork();
     if (pid1 == 0) {
-        registerProc(getpid(), getppid(), 0, 0);
+        registerProc(getpid(), getppid(), 1, 1);
 
         pid_t pid1_1 = fork();
         if (pid1_1 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 1);
             exit(0);
         }
         wait(NULL); 
@@ -36,11 +36,11 @@ void question1() {
 
     pid2 = fork();
     if (pid2 == 0) {
-        registerProc(getpid(), getppid(), 0, 0);
+        registerProc(getpid(), getppid(), 1, 2);
 
         pid_t pid2_1 = fork();
         if (pid2_1 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 2);
             exit(0); 
         }
         wait(NULL);
@@ -49,29 +49,29 @@ void question1() {
 
     pid3 = fork();
     if (pid3 == 0) {
-        registerProc(getpid(), getppid(), 0, 0);
+        registerProc(getpid(), getppid(), 1, 3);
 
         pid_t pid3_1 = fork();
         if (pid3_1 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 3);
             exit(0);
         }
 
         pid_t pid3_2 = fork();
         if (pid3_2 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 4);
             exit(0); 
         }
 
         pid_t pid3_3 = fork();
         if (pid3_3 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 5);
             exit(0);
         }
 
         pid_t pid3_4 = fork();
         if (pid3_4 == 0) {
-            registerProc(getpid(), getppid(), 0, 0);
+            registerProc(getpid(), getppid(), 2, 6);
             exit(0);
         }
 
@@ -85,5 +85,7 @@ void question1() {
     for (int i = 0; i < NB_CHILDS_LEVEL_0; i++) {
         wait(NULL);
     }
+
+    printProcRegistrations();
 }
 
