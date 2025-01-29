f5162476f4c025fc81bc3c18f7e7f2663209c73a
oltha <olivier.thabet@polymtl.ca>
2025-01-28 19:39:37 -0500

    f

--- INF2610_TP1/Section2_Processes/processes_part2.c
+++ INF2610_TP1/Section2_Processes/processes_part2.c
@@ -4,29 +4,56 @@
  * Processes - part2.c
  *
  * Popovic, Victor (2288035) et Thabet, Olivier (2294559)
-*/
+ */
 
 #include "./libprocesslab/libprocesslab.h"
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define m 1000000
-#define nb 4
+#define nb 8
 
-long somme[nb];
+long sum[nb];
 
-
-// fonction exécutée par chaque thread créé
-void* contribution(void*p)
+void *contribution(void *p)
 {
-    // TODO
-
+  int no = *(int *)p;
+  long start = (no * m / nb) + 1;
+  long end = (no + 1) * m / nb;
+  sum[no] = 0;
+  for (long i = start; i <= end; i++)
+  {
+    sum[no] += i;
+  }
   return NULL;
 }
 
-
-void question2( )
+void question2()
 {
-    // TODO
-    
-}
+  pthread_t threads[nb];
+  int threadIds[nb];
 
+  for (int i = 0; i < nb; i++)
+  {
+    threadIds[i] = i;
+    pthread_create(&threads[i], NULL, contribution, &threadIds[i]);
+  }
+  for (int i = 0; i < nb; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
+
+  long totalSum = 0;
+  for (int i = 0; i < nb; i++)
+  {
+    totalSum += sum[i];
+  }
+
+  long theoSum = (long)m * (m + 1) / 2;
+
+  printf("sum[0] + sum[1] + ... + sum[nb-1] = %ld\n", totalSum);
+  printf("m * (m + 1) / 2 = %ld\n", theoSum);
+
+
+}
