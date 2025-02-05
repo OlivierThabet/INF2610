42453c60ae6cbb730f290297904a345bf3a0cc5c
oltha <olivier.thabet@polymtl.ca>
2025-02-05 13:46:50 -0500

    baka

--- INF2610_TP1/Section3_Challenges/challenges_part1.c
+++ INF2610_TP1/Section3_Challenges/challenges_part1.c
@@ -3,49 +3,47 @@
  * Hiver 2025
  * Challenges - part1.c
  *
- * Ajoutez vos noms, prénoms et matricules
-*/
+ * Popovic, Victor (2288035) et Thabet, Olivier (2294559)
+ */
 
 #include "challenges_part1.h"
 
-int count_text_files(const char *dir_path) {
+int count_text_files(const char *dir_path)
+{
     DIR *dir = opendir(dir_path);
-    if (!dir) {
-        perror("Erreur lors de l'ouverture du répertoire");
-        return 0;
-    }
 
     struct dirent *entry;
     int total_count = 0;
 
-    while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
             continue;
         }
 
         char path[MAX_PATH_LENGTH];
         snprintf(path, sizeof(path), "%s/%s", dir_path, entry->d_name);
 
-        struct stat file_stat;
-        if (stat(path, &file_stat) == -1) {
-            continue;
-        }
+        int pid = fork();
+        if (pid == 0)
+        {
 
-        if (S_ISDIR(file_stat.st_mode)) {
-            int pid = fork();
-            if (pid == 0) {
-                // Processus enfant
-                int child_count = count_text_files(path);
-                exit(child_count);
-            } else {
-                // Processus parent
-                int status;
-                waitpid(pid, &status, 0);
-                if (WIFEXITED(status)) {
-                    total_count += WEXITSTATUS(status);
-                }
+            int child_count = count_text_files(path);
+            exit(child_count);
+        }
+        else
+        {
+
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                total_count += WEXITSTATUS(status);
             }
-        } else if (strstr(entry->d_name, ".txt")) {
+        }
+        if (strstr(entry->d_name, ".txt"))
+        {
             total_count++;
         }
     }
@@ -54,11 +52,8 @@ int count_text_files(const char *dir_path) {
     return total_count;
 }
 
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
     int total_text_files = count_text_files("root");
-    printf("Nombre total de fichiers texte: %d\n", total_text_files);
+    printf("N fichiers: %d\n", total_text_files);
 }
-
-
-
